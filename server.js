2870898f66c78f5649f4fb60f98f5f5c5da6e059
Rohit-saxena125 <rohitsaxena125@gmail.com>
2025-03-09 20:26:49 +0530

    docker push

--- server.js
+++ server.js
@@ -43,9 +43,7 @@ process.on('uncaughtException', exitHandler(1, 'Unexpected Error'));
 process.on('unhandledRejection', exitHandler(1, 'Unhandled Promise'));
 process.on('SIGTERM', exitHandler(0, 'SIGTERM'));
 process.on('SIGINT', exitHandler(0, 'SIGINT'));
-(async () => {
-  await createAdmin();
-})();
+
 connectDb()
   .then(() => {
     server.listen(port, () => {
