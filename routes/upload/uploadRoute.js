bdd2e9734b687613c304c0579400ad83d884c100
Rohit-saxena125 <rohitsaxena125@gmail.com>
2025-03-09 22:58:08 +0530

    token fix

--- routes/upload/uploadRoute.js
+++ routes/upload/uploadRoute.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const {videoUpload, profileUpload} = require('../../middlewares/upload/upload');
-const {tokenVerify} = require('../../middlewares/auth/authMiddleware');
+const {tokenVerify} = require('../../middlewares/authMiddleware');
 const uploadController = require('../../controllers/upload/uploadController');
 
 router.post('/video', tokenVerify, videoUpload, uploadController.uploadVideo);
