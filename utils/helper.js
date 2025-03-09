df1b5c46fd24d6f4c621719e976a3766d49454bc
Rohit-saxena125 <rohitsaxena125@gmail.com>
2025-03-09 22:18:58 +0530

    fix

--- utils/helper.js
+++ utils/helper.js
@@ -31,7 +31,7 @@ exports.sendEmail = async (mailAlert) => {
     };
     transporter.sendMail(mailOptions, function (error, info) {
       if (error) {
-        console.log(error);
+        console.log("error",error);
       } else {
         console.log('Email sent: ' + info.response);
       }
